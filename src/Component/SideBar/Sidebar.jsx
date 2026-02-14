713bdf2829c8e45812d2690521a97c1821c78526
Shivani Barman <shivanibarman247@gmail.com>
2026-02-14 22:33:21 +0530

    refactor: restructure App component and add Sidebar with styling

---
+++ src/Component/SideBar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Sidebar.module.css'
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sideBar}>
+        <div className={styles.sideBarIcon}>
+          <div>Resume Screening</div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
