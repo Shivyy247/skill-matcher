89692ee020a7d3cb096bc1fe7bf92739dcab2581
Shivani Barman <shivanibarman247@gmail.com>
2026-02-17 22:37:27 +0530

    feat: implement History component with styled history cards for better user interface

--- src/Component/History/History.jsx
+++ src/Component/History/History.jsx
@@ -1,12 +1,101 @@
-import React from 'react'
+import React from "react";
 import styles from "./History.module.css";
+import Skeleton from "@mui/material/Skeleton";
+
+
 
 const History = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.history}>
+      <div className={styles.historyCardBlock}>
+        <Skeleton
+          variant="rectangural"
+          sx={{ borderRadius: "20px" }}
+          width={266}
+          height={200}
+        />
 
-export default History
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+
+        <div className={styles.historyCard}>
+          <div className={styles.cardPercentage}>80%</div>
+          <h2>Fronted Developer</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
+            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
+            laborum aut est!
+          </p>
+          <p>Dated : 2006-11-08</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default History;
