421a18e9b38af7f5772ac114fac465a7d0f3ed73
Shivani Barman <shivanibarman247@gmail.com>
2026-02-17 23:06:36 +0530

    feat: enhance Admin component with layout and styling improvements

--- src/Component/Admin/Admin.jsx
+++ src/Component/Admin/Admin.jsx
@@ -1,12 +1,97 @@
-import React from 'react'
+import React from "react";
 import styles from "./Admin.module.css";
+import { Skeleton } from "@mui/material";
 
 const Admin = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.admin}>
+      <div className={styles.adminBlock}>
+        <Skeleton
+          variant="rectangural"
+          sx={{ borderRadius: "20px" }}
+          width={266}
+          height={200}
+        />
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
 
-export default Admin
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
+
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
+
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
+
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
+
+        <div className={styles.adminCard}>
+          <h2>Shivyy</h2>
+          <p style={{ color: "blue" }}>shivyyy@gmail.com</p>
+          <h3>Score : 50%</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            laborum enim quo dolores facere magnam commodi odio ratione ut
+            veritatis, porro placeat et, aliquam nostrum minus similique quos
+            aspernatur consequuntur adipisci laboriosam molestias! Id assumenda,
+            molestias laudantium corrupti fugit similique?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
