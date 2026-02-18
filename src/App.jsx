3cc3e1eb710c989bc95cfe90184ae95f55bb661d
Shivani Barman <shivanibarman247@gmail.com>
2026-02-18 13:46:13 +0530

    feat: add Login component with styling for user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Admin from './Component/Admin/Admin';
 import Dashboard from './Component/Dashboard/Dashboard'
 import History from './Component/History/History';
+import Login from './Component/Login/Login';
 import Sidebar from './Component/SideBar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 
@@ -14,6 +15,7 @@ function App() {
       <Sidebar />
       <Routes>
 
+        <Route path='/' element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<History />} />
         <Route path="/admin" element={<Admin />} />
