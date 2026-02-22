57d0c50d21854d8dd42327d2379ab01c210f32b0
Shivani Barman <shivanibarman247@gmail.com>
2026-02-22 22:46:08 +0530

    feat: implement user data fetching in History component with loading state

--- frontend/src/Component/History/History.jsx
+++ frontend/src/Component/History/History.jsx
@@ -1,98 +1,79 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styles from "./History.module.css";
 import Skeleton from "@mui/material/Skeleton";
 import WithAuthHOC from "../../utils/withAuthHOC";
+import axios from '../../utils/axios'
+import { AuthContext } from "../../utils/HOC/AuthContext";
 
 
 const History = () => {
+
+  const [data, setData] = useState([])
+  const [loader, setLoader] = useState(false)
+
+  const {userInfo} = useContext(AuthContext)
+  
+  useEffect(() => {
+    const fatchUserData = async () => {
+      setLoader(true)
+      try {
+        const results = await axios.get(`/api/resume/get/${userInfo?._id}`);
+        console.log(results.data.resumes)
+        setData(results.data.resumes);
+      } catch (error) {
+        console.log(error)
+        alert('something went wrong!')
+      }
+      finally {
+        setLoader(false)
+      }
+    }
+    fatchUserData()
+  },[])
+
   return (
     <div className={styles.history}>
       <div className={styles.historyCardBlock}>
-        <Skeleton
-          variant="rectangural"
-          sx={{ borderRadius: "20px" }}
-          width={266}
-          height={200}
-        />
+        {loader && (
+          <>
+            <Skeleton
+              variant="rectangural"
+              sx={{ borderRadius: "20px" }}
+              width={266}
+              height={200}
+            />
+            <Skeleton
+              variant="rectangural"
+              sx={{ borderRadius: "20px" }}
+              width={266}
+              height={200}
+            />
+            <Skeleton
+              variant="rectangural"
+              sx={{ borderRadius: "20px" }}
+              width={266}
+              height={200}
+            />
+            <Skeleton
+              variant="rectangural"
+              sx={{ borderRadius: "20px" }}
+              width={266}
+              height={200}
+            />
+          </>
+        )}
 
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
-
-        <div className={styles.historyCard}>
-          <div className={styles.cardPercentage}>80%</div>
-          <h2>Fronted Developer</h2>
-          <p>Resume Name : Resume.pdf</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-            fugiat excepturi fuga voluptatem magni id vel incidunt perspiciatis
-            vero dolorem voluptatibus sequi, tempora iste? Totam nam dolore
-            laborum aut est!
-          </p>
-          <p>Dated : 2006-11-08</p>
-        </div>
+        {data.map((item, index) => {
+          return (
+            <div key={item._id} className={styles.historyCard}>
+              <div className={styles.cardPercentage}>{item.score}%</div>
+              {/* <h2></h2> */}
+              <p>Resume Name : {item.resume_name}</p>
+              <p>{item.feedback}</p>
+              <p>Dated : {item.createdAt.slice(0, 10)}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
